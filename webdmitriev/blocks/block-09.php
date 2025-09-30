d09c34cee4532fbf68cbafefbd8c77962791845c
webdmitriev <webdmitriev@gmail.com>
2025-09-30 23:23:17 +0300

    block-09 and block-07 is changed

--- webdmitriev/blocks/block-09.php
+++ webdmitriev/blocks/block-09.php
@@ -10,6 +10,9 @@ $url = get_template_directory_uri();
 $image_base64 = 'data:image/gif;base64,R0lGODlhBwAFAIAAAP///wAAACH5BAEAAAEALAAAAAAHAAUAAAIFjI+puwUAOw==';
 
 $allowed_tags = array(
+  'a'    => array(
+    'href' => array(),
+  ),
   'br'    => array(),
   'span'  => array(
     'class' => array(),
